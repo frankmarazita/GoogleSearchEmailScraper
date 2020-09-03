d758a74e3cc1550b2a25b595d32c9461fb844513
Frank Marazita <fmarazita1@gmail.com>
2020-09-03 13:50:52 +1000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# GoogleSearchEmailScraper
+# Google Search Email Scraper
 A short python 3 script that finds emails on pages of google search results
+
+### Installing Dependencies
+* pip3 install -r requirements.txt
+
+### Running The Application
+
+1. Enter your search queries in 'searches.txt', each on a new line
+2. Change parameters in the search.py file to better match your requirements
+3. Run the code: python3 search.py
+
+### Notes
+
+* Keep in mind that if you are looking for specific results, there may be inconsistencies and/or false positives.
+* Sites that use Cloudflare or similar protection may or may not express results.
+* There is always the possibility of a temporary IP ban when conducting any form of web scraping.
+    * The likelihood of this can be mitigated by increasing the pause parameter variable in the code.
+* Using google search techniques found at https://support.google.com/websearch/answer/2466433?hl=en, can be helpful to use in the 'SEARCH_KEYWORDS' variable in order to better refine search results.
---
+++ search.py
@@ -0,0 +1,52 @@
+from googlesearch import search
+import requests
+import re
+import csv
+
+NUM_GOOGLE_RESULTS = 10     # Number of google search results for each search
+PAUSE = 0.5                 # Pause time in seconds between each google search
+TIMEOUT = 5                 # Seconds before the request on the domain timeouts
+DOMAIN_CONTAIN = '.com.au'  # String match in domain
+INCLUDE_PDF = False  # If enabled, can take much longer (increase timeout)
+SEARCH_KEYWORDS = " email contact vic"  # This will be appended to every search
+ENABLE_CONSOLE = True
+
+searches = []
+with open('searches.txt') as f:
+    searches = [line.rstrip('\n') for line in f]
+
+with open('output.csv', mode='w', newline='') as file_output:
+    file_writer = csv.writer(file_output, delimiter=',')
+
+    for s in searches:
+        if ENABLE_CONSOLE:
+            print("Name:", s)
+        file_writer.writerow(["Name: " + s])
+        found = False
+        try:
+            domains = search(s + SEARCH_KEYWORDS, tld="com", num=NUM_GOOGLE_RESULTS, stop=NUM_GOOGLE_RESULTS, pause=PAUSE)
+            for domain in domains:
+                if not domain.endswith(".pdf") or INCLUDE_PDF:
+                    if DOMAIN_CONTAIN in domain:
+                        data = None
+                        try:
+                            data = requests.get(domain, timeout=TIMEOUT)
+                        except:
+                            if ENABLE_CONSOLE: print("Timeout")
+                        emails = re.findall(
+                            r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
+                        # I no emails are found, skip
+                        if len(emails) > 0:
+                            file_writer.writerow([domain])
+                            file_writer.writerow(emails)
+                            if ENABLE_CONSOLE:
+                                print(domain)
+                                print(emails)
+                            found = True
+        except:
+            pass
+        if not found:
+            file_writer.writerow(["NONE FOUND"])
+        file_writer.writerow([])
+        if ENABLE_CONSOLE:
+            print("------------------------------------------------------------")
